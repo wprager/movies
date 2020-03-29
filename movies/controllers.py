0f40efcfa8bd128353b43fd9fcd8578e770bc9dc
Wesley Prager <wprager@Wesleys-MacBook-Pro.local>
2020-03-28 17:53:13 -0700

    min requirements : popular movies, search, and movie detail pages + support

--- movies/controllers.py
+++ movies/controllers.py
@@ -1,11 +1,31 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 from movies import app
 
-import requests
+import json
+import urllib.parse
+import urllib.request
+
+API_KEY = '91f2119708320f8847a2f21999aab4f3'
+API_URL = 'https://api.themoviedb.org/3/'
 
 # ---------------------------------------
 # ROUTES
 # ---------------------------------------
 @app.route('/')
 def route_index():
-	return 'hello world'
+	r = urllib.request.urlopen('{}movie/popular?api_key={}'.format(API_URL, API_KEY))
+	popular_movies = json.loads(r.read()).get('results')
+	return render_template('index.html', popular_movies=popular_movies)
+
+@app.route('/movie/<movie_id>')
+def route_movie(movie_id):
+	r = urllib.request.urlopen('{}movie/{}?api_key={}'.format(API_URL, movie_id, API_KEY))
+	movie = json.loads(r.read())
+	return render_template('movie.html', movie=movie)
+
+@app.route('/search')
+def route_search():
+	query = urllib.parse.quote(request.args.get('query'))
+	r = urllib.request.urlopen('{}search/movie?api_key={}&query={}'.format(API_URL, API_KEY, query))
+	movies = json.loads(r.read()).get('results')
+	return render_template('search-results.html', movies=movies)
